bb262a771d80f73aeba86c7e3bce90afc27c8e50
商博 AKA APTM82589933 AKA Bo Shang <simp4erosolar@gmail.com>
2024-11-27 19:25:10 -0500

    Initial Commit

---
+++ DevInfoTests/DevInfoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DevInfoTests.swift
+//  DevInfoTests
+//
+//  Created by Bo Shang on 11/27/24.
+//
+
+import Testing
+@testable import DevInfo
+
+struct DevInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
